340dcde8f0190b5f86def5c42bef2caeb81bb84e
Bharti <bhartiswamitech@gmail.com>
2020-06-04 00:34:44 +0530

    Deletion of First Element in Linked List

---
+++ myjava/src/myjava/LLDetetionAt1.java
@@ -0,0 +1,41 @@
+package myjava;
+
+class Node{
+	int data;
+	Node next;
+	Node(int data) {
+		this.data=data;
+		this.next=null;
+	}
+}
+public class LLDeletionAt1 {
+	
+	                  //Function for printing the Linked List
+	
+	static void Print(Node head)
+	{
+		if(head==null) {
+			System.out.println("Your Linked List is Empty");
+		}
+		
+	while(head!=null) {
+		System.out.print(head.data+" ");
+		head=head.next;
+	}
+	System.out.println();
+	}
+	
+                   	//Main Function
+	
+public static void main(String[] args) {
+	Node head=new Node(1);
+	head.next=new Node(2);
+	head.next.next=new Node(3);
+	head.next.next.next=new Node(4);
+	head.next.next.next.next=new Node(5);
+	Print(head);
+	               //Deleting the first element of linked List
+	head=head.next;
+	Print(head);
+}
+}
