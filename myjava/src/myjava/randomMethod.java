78d2f271f2efbc010f8d5e6745439d25a87dafd0
Bharti <bhartiswamitech@gmail.com>
2020-06-16 03:09:27 +0530

    Random method in java

---
+++ myjava/src/myjava/randomMethod.java
@@ -0,0 +1,9 @@
+package myjava;
+import java.util.*;
+
+public class randomMethod {
+	public static void main(String[] arg) {
+		System.out.println(Math.random());				//prints a random double number greater than or equal to 0 and less than 1;
+		System.out.println(Math.random());
+	}
+}
